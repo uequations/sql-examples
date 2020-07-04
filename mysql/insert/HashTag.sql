a3e4013e6a0a7520bfa22cc5dbd14bca728f565b
Mensah Alkebu-Lan <malkebu-lan@uequations.com>
2020-07-04 08:42:20 -0400

    Update HashTag.sql

--- mysql/insert/HashTag.sql
+++ mysql/insert/HashTag.sql
@@ -1,1 +1,3 @@
 INSERT INTO prisma.HashTag(hash_tag,hash_tag_type,popularity_score,hash_tag_count) VALUES ('PoliceOfficers','community',2,1);
+
+INSERT INTO prisma.HashTag(hash_tag,hash_tag_type,popularity_score,hash_tag_count) VALUES ('ElijahMcClain LawEnforcement','community',2,2);
