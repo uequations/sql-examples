8f9d3551ce6fde1596534685ee94d073fcf09df4
Mensah Alkebu-Lan <malkebu-lan@uequations.com>
2020-07-16 19:31:50 -0400

    Update HashTag.sql

--- mysql/select/HashTag.sql
+++ mysql/select/HashTag.sql
@@ -1,1 +1,1 @@
-select POW(2,DATEDIFF(updated_at,NOW()))*popularity_score*hash_tag_count as days_since_updated,hash_tag,popularity_score from prisma.HashTag order by days_since_updated DESC limit 20;
+select id,POW(2,DATEDIFF(updated_at,NOW()))*popularity_score*hash_tag_count as overall_score,hash_tag,popularity_score from prisma.HashTag order by overall_score DESC limit 20;
