7accf017192b10b536e1b37fc0d3722dac959656
Mensah Alkebu-Lan <malkebu-lan@uequations.com>
2021-01-16 21:59:11 -0500

    Update HashTag.sql

--- mysql/select/HashTag.sql
+++ mysql/select/HashTag.sql
@@ -1,1 +1,4 @@
 select id,POW(2,DATEDIFF(updated_at,NOW()))*popularity_score*hash_tag_count as overall_score,hash_tag,popularity_score from prisma.HashTag order by overall_score DESC limit 20;
+                         
+select * from prisma.HashTag where hash_tag like '%machinel%';
+
