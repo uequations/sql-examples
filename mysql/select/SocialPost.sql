7d20f640f87b024c62fab78f81926574c7438aeb
Mensah Alkebu-Lan <malkebu-lan@uequations.com>
2021-01-26 22:18:47 -0500

    Update SocialPost.sql

--- mysql/select/SocialPost.sql
+++ mysql/select/SocialPost.sql
@@ -3,3 +3,5 @@ select * from uequations.SocialPost where backlinks > 50 order by id DESC limit 
 select * from uequations.SocialPost where lower(hash_tags) like '%elijah%';
 
 select * from uequations.SocialPost where lower(hash_tags) like '%business%';
+
+select count(*) from uequations.SocialPost where lower(hash_tags) like '%#machinel%';
